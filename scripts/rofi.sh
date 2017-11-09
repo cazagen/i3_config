156fc8dec6627011bc673b58eb3ff5a7c5fbc2f1
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-11-09 21:18:46 +0000

    Can now shift+left/right to different rofi modes

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,1 +1,1 @@
-rofi -combi-modi window,drun,ssh -show combi -modi combi -padding 5 -seperator-style "none" -font "Hack 14" -width 1000 -yoffset 100
+rofi -combi-modi window,drun,ssh -show combi -modi combi,window,drun,ssh -padding 5 -seperator-style "none" -font "Hack 14" -width 1000 -yoffset 100
