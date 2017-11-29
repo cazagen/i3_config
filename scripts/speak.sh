70dd4712d7fbd1df9d59d8cf460961a2ac44f8cb
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-11-29 05:07:01 +0000

    Use py3status, update hacklab speak script, add bustracker

--- scripts/speak.sh
+++ scripts/speak.sh
@@ -1,6 +1,1 @@
-exec 3>&1;
-result=$(dialog --inputbox "Hacklab Speak" 0 0 2>&1 1>&3);
-exitcode=$?;
-exec 3>&-;
-echo $result $exitcode;
-mosquitto_pub -h mqtt -t 'sound/g1/speak' -m $result
+mosquitto_pub -h mqtt -t 'sound/g1/speak' -m "$(zenity --entry)"
