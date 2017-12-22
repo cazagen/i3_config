475ade2ca54cbba2b4864822276bfa406ca892f4
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-12-22 16:15:12 +0000

    actually add screenshot tool....

---
+++ scripts/screenshot_selection.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILENAME=$(date +"%Y-%m-%d_%H:%M")
+
+URL="https://i.cazagen.me/"$FILENAME.png
+FILENAME=$HOME/Pictures/$FILENAME.png
+
+scrot -s $FILENAME
+
+scp -i /home/cazagen/.ssh/id_nopass $FILENAME do:/home/cazagen/img
+
+echo $URL | xclip -selection c
