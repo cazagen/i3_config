414aa7624c4cc3c624cacc650eca27b9631ecf84
Ben Casling <bencasling1996@gmail.com>
2019-10-01 23:12:41 +0100

    Make scrot use a .jpg file then convert to .png (for some reason this is quicker than using a .png all the way).

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scrot /tmp/i3lock.png
-convert /tmp/i3lock.png -resize 25% /tmp/i3lock.png
+scrot /tmp/i3lock.jpg
+convert /tmp/i3lock.jpg -resize 25% /tmp/i3lock.png
 convert /tmp/i3lock.png -blur 0x2 /tmp/i3lock.png
 convert /tmp/i3lock.png -resize 400% /tmp/i3lock.png
 i3lock -i /tmp/i3lock.png
